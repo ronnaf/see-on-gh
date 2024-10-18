8f90fc9aa78c01da859ab332d03f8e5909266599
Ronna Firmo <ronna.firmo1@gmail.com>
2024-10-19 00:32:35 +0800

    feat: inject on url change

--- content.js
+++ content.js
@@ -1,24 +1,35 @@
-const interval = setInterval(() => {
-  const releaseElement = document.querySelector('div[title="release"]');
-  if (releaseElement) {
-    clearInterval(interval);
-    handleReleaseElement(releaseElement);
-  }
-}, 1000);
+function initialize() {
+  const interval = setInterval(() => {
+    const releaseElement = document.querySelector('div[title="release"]');
+    if (releaseElement) {
+      clearInterval(interval);
+      injectLink(releaseElement);
+    }
+  }, 1000);
+}
 
-function handleReleaseElement(releaseElement) {
-  const valueElement = releaseElement.parentElement?.parentElement?.querySelector('a span');
+function injectLink(releaseElement) {
+  const valueElement =
+    releaseElement.parentElement?.parentElement?.querySelector("a span");
   if (!valueElement) return;
 
   const release = valueElement.textContent;
-  const buildNumber = release?.split('.').at(0);
+  const buildNumber = release?.split(".").at(0);
 
-  const button = document.createElement('a');
-  button.style.marginLeft = '4px'
-  button.textContent = '(see on github)';
-  button.target = '_blank';
-  button.rel = 'noopener noreferrer';
-  button.href = `https://github.com/Carepatron/Carepatron-App/actions/runs/${buildNumber}`
+  const button = document.createElement("a");
+  button.style.marginLeft = "4px";
+  button.textContent = "(see on github)";
+  button.target = "_blank";
+  button.rel = "noopener noreferrer";
+  button.href = `https://github.com/Carepatron/Carepatron-App/actions/runs/${buildNumber}`;
 
   releaseElement.appendChild(button);
 }
+
+window.addEventListener("load", initialize);
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "URL_CHANGED") {
+    initialize();
+  }
+});
