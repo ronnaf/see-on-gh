193601f669103f161da0ff2d5350ba8b4c46d467
Ronna Firmo <ronna.firmo1@gmail.com>
2024-10-19 15:52:00 +0800

    chore: insert to lots of places

--- background.js
+++ background.js
@@ -1,30 +1,26 @@
 /**
- * Regex pattern to match URLs in the format:
- * `https://carepatron.sentry.io/issues/*`
- * where `*` can be any alphanumeric string and may optionally
- * be followed by a query string.
- *
- * The pattern does NOT match:
- * - URLs with additional slashes after the ID
- * - URLs that do not start with the specified base URL
- *
- * @constant {RegExp} issueUrlRegex
- * @example
- * // Matches
- * issueUrlRegex.test('https://carepatron.sentry.io/issues/5893211453'); // true
- * issueUrlRegex.test('https://carepatron.sentry.io/issues/5893211453/?environment=production&project=4506941031251968'); // true
- *
- * // Does NOT match
- * issueUrlRegex.test('https://carepatron.sentry.io/issues/5893211453/anotherSegment'); // false
- * issueUrlRegex.test('https://carepatron.sentry.io/issues/'); // false
- * issueUrlRegex.test('https://carepatron.sentry.io/issues/?environment=production'); // false
+ * match:
+ * https://carepatron.sentry.io/issues/5574468879
+ * https://carepatron.sentry.io/issues/5574468879?foo=bar
+ * https://carepatron.sentry.io/issues/5574468879/
+ * https://carepatron.sentry.io/issues/5574468879/?foo=bar
+ * https://carepatron.sentry.io/issues/5574468879/tags
+ * https://carepatron.sentry.io/issues/5574468879/tags?foo=bar
+ * https://carepatron.sentry.io/issues/5574468879/tags/
+ * https://carepatron.sentry.io/issues/5574468879/tags/?foo=bar
+ * 
+ * not match
+ * https://carepatron.sentry.io/issues/?foo=bar
+ * https://carepatron.sentry.io/issues?foo=bar
+ * https://carepatron.sentry.io/issues
  */
-const issueUrlRegex =
-  /^https:\/\/carepatron\.sentry\.io\/issues\/[^\/?]+\/?(\?.*)?$/;
+const issueUrlRegex = /^https:\/\/carepatron\.sentry\.io\/issues\/[^\/?]+(?:\/[^\/?]*)?\/?(?:\?.*)?$/i
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (tab.active && changeInfo.url && issueUrlRegex.test(changeInfo.url)) {
-    console.log(`changeInfo.url:`, changeInfo.url);
-    chrome.tabs.sendMessage(tabId, { action: 'URL_CHANGED', url: changeInfo.url });
+    chrome.tabs.sendMessage(tabId, {
+      action: "URL_CHANGED",
+      url: changeInfo.url,
+    });
   }
 });
--- content.js
+++ content.js
@@ -1,35 +1,121 @@
-function initialize() {
-  const interval = setInterval(() => {
-    const releaseElement = document.querySelector('div[title="release"]');
-    if (releaseElement) {
-      clearInterval(interval);
-      injectLink(releaseElement);
-    }
-  }, 1000);
+/**
+ * match:
+ * https://carepatron.sentry.io/issues/5574468879
+ * https://carepatron.sentry.io/issues/5574468879?foo=bar
+ * https://carepatron.sentry.io/issues/5574468879/
+ * https://carepatron.sentry.io/issues/5574468879/?foo=bar
+ * https://carepatron.sentry.io/issues/5574468879/tags
+ * https://carepatron.sentry.io/issues/5574468879/tags?foo=bar
+ * https://carepatron.sentry.io/issues/5574468879/tags/
+ * https://carepatron.sentry.io/issues/5574468879/tags/?foo=bar
+ * 
+ * not match
+ * https://carepatron.sentry.io/issues/?foo=bar
+ * https://carepatron.sentry.io/issues?foo=bar
+ * https://carepatron.sentry.io/issues
+ */
+const issueUrlRegex = /^https:\/\/carepatron\.sentry\.io\/issues\/[^\/?]+(?:\/[^\/?]*)?\/?(?:\?.*)?$/i
+
+let interval;
+function waitForPage(selector) {
+  return new Promise((resolve) => {
+    interval = setInterval(() => {
+      const releaseElements = document.querySelectorAll('div[title="release"]');
+      const seenInfoElements = document.querySelectorAll('div[data-sentry-element="HovercardWrapper"][data-sentry-component="SeenInfo"][data-sentry-source-file="seenInfo.tsx"]');
+
+      if (releaseElements.length && seenInfoElements.length) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 500);
+  });
 }
 
-function injectLink(releaseElement) {
+function createLinkElement(buildNumber) {
+  const div = document.createElement("div");
+  div.style.marginLeft = "4px";
+
+  const a = document.createElement("a");
+  a.textContent = "(see on github)";
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
+  a.href = `https://github.com/Carepatron/Carepatron-App/actions/runs/${buildNumber}`;
+
+  div.appendChild(a);
+
+  return div;
+}
+
+function injectToTreeValueElements() {
+  const releaseElement = document.querySelector('div[title="release"]');
   const valueElement =
-    releaseElement.parentElement?.parentElement?.querySelector("a span");
-  if (!valueElement) return;
+    releaseElement?.parentElement?.parentElement?.querySelector("a span");
 
-  const release = valueElement.textContent;
-  const buildNumber = release?.split(".").at(0);
+  const releaseString = valueElement?.textContent;
+  const buildNumber = releaseString?.split(".").at(0);
 
-  const button = document.createElement("a");
-  button.style.marginLeft = "4px";
-  button.textContent = "(see on github)";
-  button.target = "_blank";
-  button.rel = "noopener noreferrer";
-  button.href = `https://github.com/Carepatron/Carepatron-App/actions/runs/${buildNumber}`;
+  if (!buildNumber) {
+    console.error("Failed to find build number");
+    return;
+  }
 
-  releaseElement.appendChild(button);
+  const result = document.evaluate(
+    `//text()[contains(., '${releaseString}')]`,
+    document,
+    null,
+    XPathResult.ANY_TYPE,
+    null
+  );
+
+  let nodes = [];
+  let node = result.iterateNext();
+  while (node) {
+    nodes.push(node.parentNode);
+    node = result.iterateNext();
+  }
+
+  nodes.forEach((node) => {
+    const div = createLinkElement(buildNumber);
+    const treeValueElement = node.parentElement.parentElement.parentElement;
+    if (treeValueElement.getAttribute("data-sentry-element") === "TreeValue") {
+      treeValueElement.style.display = "flex";
+      treeValueElement.style.alignItems = "center";
+      treeValueElement.appendChild(div);
+    }
+  });
 }
 
-window.addEventListener("load", initialize);
+function injectToSeenElements() {
+  const seenInfoElements = document.querySelectorAll(
+    'div[data-sentry-element="HovercardWrapper"][data-sentry-component="SeenInfo"][data-sentry-source-file="seenInfo.tsx"]'
+  );
+  seenInfoElements.forEach((seenInfoElement) => {
+    const valueElement = seenInfoElement.querySelector("a span");
+    const releaseString = valueElement?.textContent;
+    const buildNumber = releaseString?.split(".").at(0);
+    const div = createLinkElement(buildNumber);
+    div.style.marginLeft = 0;
+    seenInfoElement.parentElement.appendChild(div);
+  });
+}
+
+async function initialize() {
+  clearInterval(interval);
+
+  await waitForPage();
+
+  injectToTreeValueElements();
+  injectToSeenElements();
+}
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "URL_CHANGED") {
+    initialize();
+  }
+});
+
+window.addEventListener("load", () => {
+  if (issueUrlRegex.test(window.location.href)) {
     initialize();
   }
 });
